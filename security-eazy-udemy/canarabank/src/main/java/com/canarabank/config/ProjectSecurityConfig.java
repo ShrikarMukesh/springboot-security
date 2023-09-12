0fc29ec47b1c4f8c8675f917e9a16dbb6724f13f
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2023-09-12 21:45:38 +0530

    DataSource Implementation completed

--- security-eazy-udemy/canarabank/src/main/java/com/canarabank/config/ProjectSecurityConfig.java
+++ security-eazy-udemy/canarabank/src/main/java/com/canarabank/config/ProjectSecurityConfig.java
@@ -1,10 +1,20 @@
 package com.canarabank.config;
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -16,9 +26,17 @@ public class ProjectSecurityConfig {
          *  Below is the custom security configurations
          */
 
-        http.authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll())
+//        http.authorizeHttpRequests((requests) ->
+//                        requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+//                        .requestMatchers("/notices","/contact").permitAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+//        return http.build();
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests)->requests
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
@@ -38,5 +56,61 @@ public class ProjectSecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();*/
+    }
+
+//    @Bean
+//    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+//        /*
+//        Approach 1 where we use withDefaultPasswordEncoder() method
+//        while creating the user details
+//
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user1 = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//
+//        UserDetails user2 = User.withDefaultPasswordEncoder()
+//                .username("paul")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        */
+//
+//
+//        //Approach 2 where we use NoOpPasswordEncoder Bean
+//		//while creating the user details
+//
+//        UserDetails admin = User.withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user1 = User.withUsername("user1")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//
+//        UserDetails user2 = User.withUsername("user2")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        //InMemoryUserDetailsManager Accepts the Variable argument constructor
+//        return new InMemoryUserDetailsManager(admin,user1,user2);
+//    }
+
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
