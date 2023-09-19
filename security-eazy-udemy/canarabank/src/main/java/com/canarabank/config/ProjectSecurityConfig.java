f8e3d987294cceb5e6b15b38779aa619f0f05170
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2023-09-19 16:05:32 +0530

    Done Password Management in Spring Security with PasswordEncoders

--- security-eazy-udemy/canarabank/src/main/java/com/canarabank/config/ProjectSecurityConfig.java
+++ security-eazy-udemy/canarabank/src/main/java/com/canarabank/config/ProjectSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class  ProjectSecurityConfig {
-
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
